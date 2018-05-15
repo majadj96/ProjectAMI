9cedc9c02bc48130f505c20dc47e352e800960db
Maja <maya.simsa1@gmail.com>
2018-05-15 22:36:40 +0200

    dodata klasa Device u AMIDevice

---
+++ AMIDevice/Device.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMIDevice
+{
+    class Device
+    {
+
+        private string DeviceCode;
+
+        public Device()
+        {
+            DeviceCode = "1";
+
+        }
+
+    }
+}
