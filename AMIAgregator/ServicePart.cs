a9debc669d133a241801685ae7a7251db8817635
Maja <maya.simsa1@gmail.com>
2018-05-22 17:05:34 +0200

    Dodat WCF servis kod agregatora

---
+++ AMIAgregator/ServicePart.cs
@@ -0,0 +1,31 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMIAgregator
+{
+    public class ServicePart
+    {
+        private static ServiceHost serviceHost;
+        public ServicePart()
+        {
+            serviceHost = new ServiceHost(typeof(Agregator));
+            serviceHost.AddServiceEndpoint(typeof(IAMIAgregator), new NetTcpBinding(), new Uri("net.tcp://localhost:8000/IAMIAgregator"));
+        }
+
+        public void Open()
+        {
+            serviceHost.Open();
+        }
+
+        public void Close()
+        {
+            serviceHost.Close();
+        }
+
+    }
+}
