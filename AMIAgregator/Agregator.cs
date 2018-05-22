12ee19e8b53702fc44d36deb785151449a0bf4dd
Maja <maya.simsa1@gmail.com>
2018-05-22 16:08:14 +0200

    Gotov konacno IAMIAgregator

--- AMIAgregator/Agregator.cs
+++ AMIAgregator/Agregator.cs
@@ -4,14 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Common.Enums;
 
 namespace AMIAgregator
 {
     class Agregator : IAMIAgregator
     {
         public string agregatorCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Enums.State state { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-        public bool Send(string code, DateTime timestamp, System.Collections.Generic.Dictionary<object> )
+        public bool Send(string code, DateTime timestamp, Dictionary<MeasureType, double> measurements);
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Send(string code, DateTime timestamp, Dictionary<Enums.MeasureType, double> measurements)
         {
             throw new NotImplementedException();
         }
