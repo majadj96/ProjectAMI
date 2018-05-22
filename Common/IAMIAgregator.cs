f4d9db531938fe6501465ebcbb44d50ec485b2f6
Maja <maya.simsa1@gmail.com>
2018-05-22 15:53:26 +0200

    Dodato - Interfejs za agregator i Datas klasa

---
+++ Common/IAMIAgregator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public interface IAMIAgregator
+    {
+
+        string agregatorCode { get; set; }
+
+
+        void turnOn();
+        void turnOff();
+        bool Send(string code, DateTime timestamp, Dictionary<)
+        
+
+    }
+}
