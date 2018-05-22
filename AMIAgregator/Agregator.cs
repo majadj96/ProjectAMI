0ba689061f32b08bdbdfbc280b6585b06a830d78
Maja <maya.simsa1@gmail.com>
2018-05-22 16:03:45 +0200

    Nasledila sam IAMIAgregator

--- AMIAgregator/Agregator.cs
+++ AMIAgregator/Agregator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace AMIAgregator
 {
-    class Agregator
+    class Agregator : IAMIAgregator
     {
+        public string agregatorCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool Send(string code, DateTime timestamp, System.Collections.Generic.Dictionary<object> )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void turnOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void turnOn()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
